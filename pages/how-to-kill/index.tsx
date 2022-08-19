45fd0e9c188ab7fc3ebbee6d130706170737048c
samarth44600 <subedi42samarth@gmail.com>
2022-08-19 20:39:04 +0545

    Demo Project Initialized

---
+++ pages/how-to-kill/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./howTo.module.scss";
+type Props = {};
+
+const howToKill = (props: Props) => {
+  return (
+    <div className={styles.howToDiv}>
+      <h1>How To Kill someone who is looking at my screen?</h1>
+    </div>
+  );
+};
+
+export default howToKill;
